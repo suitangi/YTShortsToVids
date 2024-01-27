31bb70cc68a59b0808996f32a3fa286534ead05a
Suitangi Uil <suitangi777@gmail.com>
2024-01-26 18:22:12 -0800

    Updated to latest js spec

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,37 +1,32 @@
-var oldHref = document.location.href;
 checkURL();
-
 
 //function to check url and redirect if needed
 function checkURL() {
   let l = location.href;
   if (l.startsWith('https://www.youtube.com/shorts/')) {
-    console.log('YTShorts2Vids (' + chrome.runtime.getManifest().version + ') URL Redirected');
-    console.log('Old URL: ' + l);
+    console.log(`YTShorts2Vids (${chrome.runtime.getManifest().version}) URL Redirected`);
+    console.log(`Old URL: ${l}`);
     l = l.replace('https://www.youtube.com/shorts/', '');
     l = 'https://www.youtube.com/watch?v=' + l;
-    console.log('New URL:' + l);
+    console.log(`New URL: ${l}`);
     window.location.replace(l);
   }
 }
 
 //on link change, check url again
-window.onload = function() {
-  var bodyList = document.querySelector("body")
-
-  var observer = new MutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-      if (oldHref != document.location.href) {
-        oldHref = document.location.href;
-        checkURL();
-      }
-    });
+const observeUrlChange = () => {
+  let oldHref = document.location.href;
+  const body = document.querySelector("body");
+  const observer = new MutationObserver(mutations => {
+    if (oldHref !== document.location.href) {
+      oldHref = document.location.href;
+      checkURL();
+    }
   });
-
-  var config = {
+  observer.observe(body, {
     childList: true,
     subtree: true
-  };
-
-  observer.observe(bodyList, config);
+  });
 };
+
+window.onload = observeUrlChange;
